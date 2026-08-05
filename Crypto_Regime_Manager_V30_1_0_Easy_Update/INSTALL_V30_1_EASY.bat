91e806af48d5647df498bdbba9cfcc0cc2689caa
aferguson227 <a.ferguson227@gmail.com>
2026-08-05 08:50:45 +0100

    Update V30.2

--- Crypto_Regime_Manager_V30_1_0_Easy_Update/INSTALL_V30_1_EASY.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "%~dp0INSTALL_V30_1_EASY.ps1"
-Pause
--- Crypto_Regime_Manager_V30_1_0_Easy_Update/RUN_V30_1_KRAKEN_RESEARCH.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "%~dp0RUN_V30_1_KRAKEN_RESEARCH.ps1"
